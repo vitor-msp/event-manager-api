e85c9b343b3c45278ba638c55ea62c3f3f1734e9
Vitor Mateus <vitor7jan@gmail.com>
2022-05-17 20:25:00 -0300

    initial config ts/jest

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+export default {
+  bail: 1,
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**",
+    "!src/**/I*.ts",
+    "!src/**/*DTO.ts",
+    "!src/server.ts",
+  ],
+  coverageDirectory: "test/coverage",
+  coverageProvider: "v8",
+  preset: "ts-jest",
+  testMatch: [
+    "**/test/**/*.test.ts?(x)",
+  ],
+};
