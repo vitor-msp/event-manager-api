9706e4a56ec52de602b4175ec89af5341188ee88
Vitor Mateus <vitor7jan@gmail.com>
2022-05-20 20:58:11 -0300

    ensure invalid request when invalid start

--- test/integration/validators/CreateEventValidator.test.ts
+++ test/integration/validators/CreateEventValidator.test.ts
@@ -24,7 +24,7 @@ describe("Create Event Validator", () => {
     expect(res.body).toEqual(errorResponse);
   });
 
-  it("should return invalid request error title not string", async () => {
+  it("should return invalid request error invalid title", async () => {
     const reqBody = {
       title: 1,
     };
@@ -40,7 +40,7 @@ describe("Create Event Validator", () => {
     expect(res.body).toEqual(errorResponse);
   });
 
-  it("should return invalid request error title not string", async () => {
+  it("should return invalid request error missing start", async () => {
     const reqBody = {
       title: "Event Test",
     };
@@ -51,6 +51,23 @@ describe("Create Event Validator", () => {
 
     const errorResponse: ErrorResponse = {
       message: "Missing Start",
+    };
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toEqual(errorResponse);
+  });
+
+  it("should return invalid request error invalid start", async () => {
+    const reqBody = {
+      title: "Event Test",
+      start: "1"
+    };
+
+    const res: request.Response = await request(app)
+      .post("/event")
+      .send(reqBody);
+
+    const errorResponse: ErrorResponse = {
+      message: "Invalid Start",
     };
     expect(res.statusCode).toBe(400);
     expect(res.body).toEqual(errorResponse);
