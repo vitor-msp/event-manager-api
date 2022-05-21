efac46108953b7a861b2935e0bc91ea720084cbe
Vitor Mateus <vitor7jan@gmail.com>
2022-05-20 23:54:50 -0300

    ensure receive bad request for invalid duration when edit event

--- test/integration/validators/EditEventValidator.test.ts
+++ test/integration/validators/EditEventValidator.test.ts
@@ -28,7 +28,6 @@ describe("Edit Event Validator", () => {
 
   it("should return invalid request error invalid start", async () => {
     const reqBody = {
-      title: "Event Test",
       start: "1",
     };
 
@@ -43,6 +42,21 @@ describe("Edit Event Validator", () => {
     expect(res.body).toEqual(errorResponse);
   });
 
+  it("should return invalid request error invalid duration", async () => {
+    const reqBody = {
+      duration: "a",
+    };
+
+    const res: request.Response = await request(app)
+      .put("/event")
+      .send(reqBody);
+
+    const errorResponse: ErrorResponse = {
+      message: "Invalid Duration",
+    };
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toEqual(errorResponse);
+  });
 
   afterAll(async () => {
     mongoose.disconnect();
