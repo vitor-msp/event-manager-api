1588b1a16e262bcd08bd9d1135143c8004f80777
Vitor Mateus <vitor7jan@gmail.com>
2022-05-20 23:55:48 -0300

    ensure receive bad request for invalid guest(missing user) when edit event

--- test/integration/validators/EditEventValidator.test.ts
+++ test/integration/validators/EditEventValidator.test.ts
@@ -58,6 +58,23 @@ describe("Edit Event Validator", () => {
     expect(res.body).toEqual(errorResponse);
   });
 
+  it("should return invalid request error invalid guest (missing user)", async () => {
+    const reqBody = {
+      guests: [{}],
+    };
+
+    const res: request.Response = await request(app)
+      .put("/event")
+      .send(reqBody);
+
+    const errorResponse: ErrorResponse = {
+      message: "Invalid Guest",
+    };
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toEqual(errorResponse);
+  });
+
+
   afterAll(async () => {
     mongoose.disconnect();
     app = null;
