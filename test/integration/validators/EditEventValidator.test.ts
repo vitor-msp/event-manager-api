963003478affecd27943fd0a21c6fd8ad38592e9
Vitor Mateus <vitor7jan@gmail.com>
2022-05-21 00:40:24 -0300

    edit event validator: missing event id

--- test/integration/validators/EditEventValidator.test.ts
+++ test/integration/validators/EditEventValidator.test.ts
@@ -58,6 +58,20 @@ describe("Edit Event Validator", () => {
     expect(res.body).toEqual(errorResponse);
   });
 
+  it("should return invalid request error missing event id", async () => {
+    const reqBody = {};
+    const res: request.Response = await request(app)
+      .put("/event")
+      .query({ userId: "1" })
+      .send(reqBody);
+
+    const errorResponse: ErrorResponse = {
+      message: "Missing Event Id",
+    };
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toEqual(errorResponse);
+  });
+
   afterAll(async () => {
     mongoose.disconnect();
     app = null;
