ffa33bb87a7e7ece06008072cdab2848e0a29a99
Vitor Mateus <vitor7jan@gmail.com>
2022-05-25 23:37:42 -0300

    exit of the event: should receive unauthorized when creator try exit of an event

--- test/integration/useCases/ExitOfTheEventUseCase.test.ts
+++ test/integration/useCases/ExitOfTheEventUseCase.test.ts
@@ -65,23 +65,23 @@ describe("Exit Of The Event Use Case", () => {
     expect(res.body).toEqual(errorResponse);
   });
 
-  // it("should receive unauthorized when creator try exit of an event", async () => {
-  //   await saveEvent();
-  //   const reqBody = {
-  //     eventId: 1,
-  //   };
+  it("should receive unauthorized when creator try exit of an event", async () => {
+    await saveEvent();
+    const reqBody = {
+      eventId: 1,
+    };
 
-  //   const res: request.Response = await request(app)
-  //     .put("/event/exit")
-  //     .query({ userId: "3" })
-  //     .send(reqBody);
+    const res: request.Response = await request(app)
+      .put("/event/exit")
+      .query({ userId: "1" })
+      .send(reqBody);
 
-  //   const errorResponse: ErrorResponse = {
-  //     message: "User Cannot Cancel Event",
-  //   };
-  //   expect(res.statusCode).toBe(401);
-  //   expect(res.body).toEqual(errorResponse);
-  // });
+    const errorResponse: ErrorResponse = {
+      message: "Creator Cannot Exit Of The Event!",
+    };
+    expect(res.statusCode).toBe(401);
+    expect(res.body).toEqual(errorResponse);
+  });
 
   // it("should receive ok when viewer exit of an event", async () => {
   //   await saveEvent();
