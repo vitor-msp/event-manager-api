1b399fcb0894c5736ad75e066a0a76cada970e23
Vitor Mateus <vitor7jan@gmail.com>
2022-05-28 00:28:41 -0300

    create user validator: missing user name

---
+++ usersModule/test/integration/validators/CreateUserValidator.test.ts
@@ -0,0 +1,36 @@
+import request from "supertest";
+import express from "express";
+import { App } from "../../../../main/app";
+import { CreateUserInputDto } from "../../../src/app/useCases/CreateUserInputDto";
+import { ErrorResponse } from "../../../../helpers/responses/httpResponses";
+import mongoose from "mongoose";
+
+describe("Create User Validator", () => {
+  let app: express.Application | null;
+  beforeAll(async () => {
+    app = new App().express;
+  });
+
+  it("should return bad request: missing user name", async () => {
+    // @ts-ignore
+    const reqBody: CreateUserInputDto = {
+      email: "teste@teste.com",
+      password: "teste123",
+    };
+
+    const res: request.Response = await request(app)
+      .post("/user")
+      .send(reqBody);
+
+    const errorResponse: ErrorResponse = {
+      message: "Missing User Name",
+    };
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toEqual(errorResponse);
+  });
+
+  afterAll(async () => {
+    mongoose.disconnect();
+    app = null;
+  });
+});
