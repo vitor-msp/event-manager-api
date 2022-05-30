e725163bbba99bd52715e329331f31f619a94c9d
Vitor Mateus <vitor7jan@gmail.com>
2022-05-30 19:47:05 -0300

    edit user use case test: clean unused imports

--- modules/users/test/integration/useCases/EditUserUseCase.test.ts
+++ modules/users/test/integration/useCases/EditUserUseCase.test.ts
@@ -1,7 +1,6 @@
 import request from "supertest";
 import express from "express";
 import { App } from "../../../../../main/app";
-import { CreateUserInputDto } from "../../../src/app/useCases/CreateUser/CreateUserInputDto";
 import { dataSource } from "../../../src/main/factory";
 import { UserEntity } from "../../../src/infra/database/schemas/UserEntity";
 import { ErrorResponse } from "../../../../../helpers/responses/httpResponses";
