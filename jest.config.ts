70ec6c8d91407eed572d1a2fe83b015aa000210a
Vitor Mateus <vitor7jan@gmail.com>
2022-06-01 23:56:54 -0300

    adjustment jest.config to coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,20 +1,22 @@
 export default {
   bail: 1,
   clearMocks: true,
-  // collectCoverage: true,
+  collectCoverage: true,
   collectCoverageFrom: [
     // "modules/users/src/**",
     "!**/I*.ts",
     "!**/*Dto.ts",
-    // "!**/*Data.ts",
-    "!main/**",
+    "!**/domain/types/*Data.ts",
+    "!*.*",
+    "!main/server.ts",
+    "!**/*.mock.ts",
   ],
   coverageDirectory: "test/coverage",
   coverageProvider: "v8",
   preset: "ts-jest",
   testMatch: [
     // "**/modules/users/test/integration/**/*.test.ts?(x)",
-    "**/CreateUserUseCase.test.ts",
-    // "**/events/test/**/*.test.ts?(x)",
+    // "**/CreateUserUseCase.test.ts",
+    "**/**/*.test.ts?(x)",
   ],
 };
